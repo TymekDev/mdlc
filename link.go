f4bb53d9a16c828e0890b526ea3e8bede040ae92
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-06-08 21:16:37 +0200

    fix: return error message

--- link.go
+++ link.go
@@ -17,7 +17,7 @@ type Link struct {
 func (l Link) check() (int, string) {
 	resp, err := http.Head(l.Destination)
 	if err != nil {
-		return 0, ""
+		return 0, err.Error()
 	}
 	if url := resp.Request.URL.String(); url != l.Destination {
 		return resp.StatusCode, fmt.Sprintf("indirect URL to: %s", url)
